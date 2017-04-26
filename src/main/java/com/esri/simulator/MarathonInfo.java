293495b8551c02b62d92d9b2e6bb8c8a8a45f5b8
David Jennings <djennings@esri.com>
2017-04-26 16:07:13 -0500

    Modified MarthonInfo for Elastic Framework DCOS

--- src/main/java/com/esri/simulator/MarathonInfo.java
+++ src/main/java/com/esri/simulator/MarathonInfo.java
@@ -7,11 +7,23 @@ package com.esri.simulator;
  */
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import org.apache.http.Header;
 import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLContextBuilder;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.json.JSONArray;
@@ -110,7 +122,6 @@ public class MarathonInfo {
                         brokers += addresses.getString(i);
                     }
 
-                    
                 } catch (Exception e) {
                     brokers = "";
                 }
@@ -126,7 +137,7 @@ public class MarathonInfo {
         return brokers;
     }
 
-    public String getElasticSearchTransportAddresses(String esFrameworkName)  {
+    public String getElasticSearchTransportAddresses(String esFrameworkName) {
         // Get the Transport Addresses for given Elasticsearch Framework Name (e.g. elasticsearch by default)
         String addresses = "";
 
@@ -202,7 +213,7 @@ public class MarathonInfo {
         return addresses;
     }
 
-    public String getElasticSearchHttpAddresses(String esAppName)  {
+    public String getElasticSearchHttpAddresses(String esAppName) {
         // Get the Http Addresses for given Elasticsearch Framework Name (e.g. elasticsearch by default)
 
         String addresses = "";
@@ -274,12 +285,12 @@ public class MarathonInfo {
         } catch (Exception e) {
             addresses = "Could not find elasticsearch web addresses.";
         }
-        
+
         System.out.println("elastic web: " + addresses);
         return addresses;
     }
 
-    public String getElasticSearchClusterName(String esAppName)  {
+    public String getElasticSearchClusterName(String esAppName) {
         // Get the Cluster Name for given Elasticsearch Framework Name (e.g. elasticsearch by default)
 
         String clusterName = "";
@@ -347,30 +358,165 @@ public class MarathonInfo {
 
     }
 
+    public String getElastic5Info(String esAppName, String username, String password) {
 
+
+        JSONObject returnJson = new JSONObject();
+
+        try {
+            // Get the application endoint 
+            String url = "http://marathon.mesos:8080/v2/apps/" + esAppName;
+
+            SSLContextBuilder builder = new SSLContextBuilder();
+            builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
+                    builder.build());
+            CloseableHttpClient httpclient = HttpClients.custom().setSSLSocketFactory(
+                    sslsf).build();
+
+            HttpGet request = new HttpGet(url);
+
+            HttpResponse response = httpclient.execute(request);
+            BufferedReader rd = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent()));
+
+            StringBuffer result = new StringBuffer();
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+
+            //System.out.println(result);
+            JSONObject json = new JSONObject(result.toString());
+            JSONObject app = json.getJSONObject("app");
+            JSONArray tasks = app.getJSONArray("tasks");
+            JSONObject task = tasks.getJSONObject(0);
+            JSONArray ports = task.getJSONArray("ports");
+
+            int port = ports.getInt(0);
+            String eip = task.getString("host");
+
+            returnJson.put("satApp", eip + ":" + port);
+
+            url = "http://" + eip + ":" + port + "/v1/endpoints/data";;
+
+            request = new HttpGet(url);
+
+            response = httpclient.execute(request);
+            rd = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent()));
+
+            result = new StringBuffer();
+            line = "";
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+
+            json = new JSONObject(result.toString());
+            String address = json.getJSONArray("address").getString(0);
+            String vip = json.getString("vip");
+
+            returnJson.put("address", address);
+            returnJson.put("vip", vip);
+
+            url = "http://" + address;
+
+            SSLContext sslContext = SSLContext.getInstance("SSL");
+
+            CredentialsProvider provider = new BasicCredentialsProvider();
+            UsernamePasswordCredentials credentials
+                    = new UsernamePasswordCredentials(username, password);
+            provider.setCredentials(AuthScope.ANY, credentials);
+
+            sslContext.init(null, new TrustManager[]{new X509TrustManager() {
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    System.out.println("getAcceptedIssuers =============");
+                    return null;
+                }
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] certs,
+                        String authType) {
+                    System.out.println("checkClientTrusted =============");
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] certs,
+                        String authType) {
+                    System.out.println("checkServerTrusted =============");
+                }
+            }}, new SecureRandom());
+
+            httpclient = HttpClients
+                    .custom()
+                    .setDefaultCredentialsProvider(provider)
+                    .setSSLContext(sslContext)
+                    .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+                    .build();
+
+            request = new HttpGet(url);
+            response = httpclient.execute(request);
+            rd = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent()));
+
+            Header contentType = response.getEntity().getContentType();
+            String ct = contentType.getValue().split(";")[0];
+
+            int responseCode = response.getStatusLine().getStatusCode();
+
+            result = new StringBuffer();
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+
+            json = new JSONObject(result.toString());
+
+            String clusterName = json.getString("cluster_name");
+
+            returnJson.put("cluster_name", clusterName);
+
+            // Get Endpoints 172.17.2.5:17962/v1/endpoints/data  (vip name)
+            // Use address to get Cluster Name
+        } catch (Exception e) {
+            returnJson.put("error", e.getMessage());
+        }
+
+        System.out.println(returnJson);
+        
+        return returnJson.toString();
+
+    }
 
     public static void main(String[] args) {
 
         int numargs = args.length;
 
-        if (numargs != 2) {
-            System.err.print("Usage: MarathonInfo <kafka|elastic> <framework-name>\n");
+        if (numargs != 2 && numargs != 4) {
+            System.err.print("Usage: MarathonInfo <kafka|elastic> <framework-name> (<username> <password>)\n");
         } else {
 
             MarathonInfo t = new MarathonInfo();
-            
+
             String typ = args[0];
-            
+
             if (typ.equalsIgnoreCase("kafka")) {
                 t.getBrokers(args[1]);
             } else if (typ.equalsIgnoreCase("elastic")) {
                 t.getElasticSearchClusterName(args[1]);
                 t.getElasticSearchHttpAddresses(args[1]);
-                t.getElasticSearchTransportAddresses(args[1]);                
+                t.getElasticSearchTransportAddresses(args[1]);
+            } else if (typ.equalsIgnoreCase("elastic5")) {
+                if (numargs == 2) {
+                    t.getElastic5Info(args[1], "elastic", "changeme");
+                } else {
+                    t.getElastic5Info(args[1], args[2], args[3]);
+                }
+
             } else {
                 System.err.println("First parameter should be <kafka|elastic>");
                 System.err.print("Usage: MarathonInfo <kafka|elastic> <framework-name>\n");
-            }                                  
+            }
 
         }
 
