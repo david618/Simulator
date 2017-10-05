1bf0f6b2f6658103e52fe965253e0d3bc4d18efd
David Jennings <djennings@esri.com>
2017-10-05 13:04:30 -0500

    Added csv to json converter for Planes

---
+++ pythonScripts/csv2jsonPlanes.py
@@ -0,0 +1,46 @@
+import json
+import re
+
+
+
+if __name__ == '__main__':
+    print "Start"
+
+
+    PATTERN = re.compile(r'''((?:[^,"']|"[^"]*"|'[^']*')+)''')
+
+    fin = open("planes00001")
+    fout = open("planes00001.json", "w")
+
+    for line in fin:
+        
+        #print line
+        #field = line.strip().split(",")
+	field = PATTERN.split(line.strip())[1::2]
+	#print field
+
+        row = {}
+
+        row['id'] = int(field[0])
+        row['ts'] = long(field[1])
+        row['speed'] = float(field[2])
+        row['dist'] = float(field[3])
+        row['bearing'] = float(field[4])
+        row['rtid'] = int(field[5])
+        row['orig'] = field[6]
+        row['dest'] = field[7]
+        row['secsToDep'] = int(field[8])
+        row['lon'] = float(field[9])
+        row['lat'] = float(field[10])
+
+                
+        jsonStr = json.dumps(row)
+
+        #print jsonStr
+        fout.write(jsonStr + "\n")
+
+    fin.close()
+    fout.close()
+
+
+        
